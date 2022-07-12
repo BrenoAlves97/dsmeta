94dd49b0385aab646374aa6676b1245a82fac836
Breno Alves <breno_ja@hotmail.com>
2022-07-12 20:15:33 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmesta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.instagram.com/brenoalvess97"> @brenoalvess97</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
